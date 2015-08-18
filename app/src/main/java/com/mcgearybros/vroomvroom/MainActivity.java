45ec8feb12b485dccdb2b06e4a3eb430b797b68a
Lewis McGeary <lewismcgeary@gmail.com>
2015-08-18 21:23:51 +0100

    start menu functionality. change title and close drawer when selected

--- app/src/main/java/com/mcgearybros/vroomvroom/MainActivity.java
+++ app/src/main/java/com/mcgearybros/vroomvroom/MainActivity.java
@@ -2,10 +2,13 @@ package com.mcgearybros.vroomvroom;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -30,6 +33,19 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_main);
+        toolbar.setTitle(R.string.app_name);
+        final DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        NavigationView navigationDrawer = (NavigationView) findViewById(R.id.navigation_drawer);
+        navigationDrawer.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                    toolbar.collapseActionView();
+                    toolbar.setTitle(menuItem.getTitle());
+                    drawerLayout.closeDrawers();
+                return false;
+            }
+        });
 
 
         mTitle = getTitle();
